85052006c550ddfbc019f770cf3cee1714874a33
AronNemeth <aron.nemeth8@gmail.com>
2024-04-25 15:57:00 +0200

    query called in flask process

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask  # , jsonify, request
-import psycopg2
-from config import load_config
+from flask import Flask, request, render_template  # , jsonify
+from query import query_series, query_parents_guide
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello():
-    return "Hello flask"
+def home():
+    return render_template("index.html")
 
 
-@app.route("/query_db", methods=["GET"])
+# Itt majd a frontendnek kéne küldenie adatot, amit a request.get()-en keresztül az args-hoz rendelünk
+@app.route("/query-data")  # , methods=["GET"]
 def query_get():
-    pass
+    # args = request.get()  # itt majd az args-al kell csinálni a query-t
+
+    df = query_series("Game of Thrones")
+
+    return df
 
 
 if __name__ == "__main__":
--- query.py
+++ query.py
@@ -2,14 +2,14 @@ import pandas as pd
 from connect_db import conn_db
 
 
-def query_series(title: str) -> dict:
+def query_series(title: str):
     """Queries for one TV series based on title form the series table in postgreSQL
 
     Args:
         title (str): title of the series
 
     Returns:
-        RealDictRow: the row of the specified series as dict
+        pd.DataFrame: a df with one row
     """
 
     sql_query = """
@@ -20,36 +20,38 @@ def query_series(title: str) -> dict:
     conn, cur = conn_db()
     cur.execute(sql_query, (title,))
     row = cur.fetchone()
-
     cur.close()
     conn.close()
 
-    return row
+    df = pd.DataFrame([row])
+
+    return df
 
 
-def query_parents_guide(s_id: str) -> dict:
-    """_summary_
+def query_parents_guide(s_id: str):
+    """Queries for rows in parents_guide table based in series id
 
     Args:
         s_id (str): series id (retrieved from series table)
 
     Returns:
-        dict: _description_
+        pd.DataFrame: a df with 5 rows (or 4 for one series)
     """
 
     sql_query = """
-        SELECT * FROM series
-        WHERE title = %s;
+        SELECT * FROM parents_guides
+        WHERE series_id = %s;
     """
 
     conn, cur = conn_db()
     cur.execute(sql_query, (s_id,))
-    row = cur.fetchone()
-
+    rows = cur.fetchall()
     cur.close()
     conn.close()
 
-    return row
+    df = pd.DataFrame(rows)
+
+    return df
 
 
 if __name__ == "__main__":
