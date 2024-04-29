196347a38c4a9a731cfb721ecaf41c8ced8fe17b
AronNemeth <aron.nemeth8@gmail.com>
2024-04-29 16:49:29 +0200

    minor changes due to unsuccessful st deploy

--- connect_db.py
+++ connect_db.py
@@ -3,9 +3,6 @@ import psycopg2.extras
 import boto3
 import json
 
-import os
-from dotenv import load_dotenv
-
 
 def conn_rds_db():
     """Connects to a postgreSQL DB in AWS RDS
@@ -27,35 +24,6 @@ def conn_rds_db():
             host=secret_dict["host"],
             port=secret_dict["port"],
             database=secret_dict["dbname"],
-        )
-
-        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-
-        return conn, cur
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error while connecting to PostgreSQL:", error)
-        return None, None
-
-
-def conn_local_db():
-    """Connects to a postgreSQL DB with configurations specified as environmental variables
-    Cursor is set to return rows as dictionaries
-
-    Returns:
-        conn: represents a connection to a postgreSQL DB (dtype: class)
-        cur: used to interact with a DB, allows to execute SQL commands (dtype: class)
-    """
-
-    load_dotenv()
-
-    try:
-        conn = psycopg2.connect(
-            user=os.getenv("USER"),
-            password=os.getenv("PASSWORD"),
-            host=os.getenv("HOST"),
-            port=os.getenv("PORT"),
-            database=os.getenv("DATABASE"),
         )
 
         cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
--- visualization.py
+++ visualization.py
@@ -1,4 +1,3 @@
-import pandas as pd
 import altair as alt
 import queries as q
 
